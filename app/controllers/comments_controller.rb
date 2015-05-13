b35e0e21c7a128bf5e11d4aed89f0f40a208029d
manijain <manijain333@gmail.com>
2015-05-14 02:09:58 +0530

    changes added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,11 @@ class CommentsController < ApplicationController
 	before_action :check_valid_user, only: :destroy
 
 	def create
+
 		@article = Article.find(params[:article_id])
-		@comment = @article.comments.create(comment_params)
+				
+		@comment = @article.comments.create(:description => params[:comment][:description], :article_id => params[:article_id], :parent_id => params[:parent_id], :user_id => current_user.id)
+	
 		redirect_to article_path(@article)
 	end
 
@@ -19,7 +22,7 @@ class CommentsController < ApplicationController
 	private
 
 	def comment_params
-		params.require(:comment).permit(:description, :user_id => current_user.id)
+		params.require(:comment).permit(:description, :parent_id, :user_id => current_user.id)
 	end
 
 	def require_user
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,5 @@ class Comment < ActiveRecord::Base
 	belongs_to :article
 	belongs_to :user
 
-	has_many :parents, class_name: "Comment",
-                          foreign_key: "parent_id"
-  	belongs_to :parent, class_name: "Comment"
+	has_many :parents, class_name: "Comment", foreign_key: "parent_id"
 end
